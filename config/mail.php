c7044e05ccfac67c941bfd3c3cb4d1c4e81b9bea
samira-2002-code <samiraassemlali2@gmail.com>
2026-06-26 12:46:45 +0100

    chore: clean and organize PostController logic

--- config/mail.php
+++ config/mail.php
@@ -16,6 +16,8 @@ return [
 
     'default' => env('MAIL_MAILER', 'log'),
 
+
+    
     /*
     |--------------------------------------------------------------------------
     | Mailer Configurations
